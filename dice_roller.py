11b46684420da6a0181bd9596038e466652cacaa
uwumastermonster <oscaritue@gmail.com>
2021-10-30 19:28:36 +0200

    Tutorial complete

--- dice_roller.py
+++ dice_roller.py
@@ -1,11 +1,13 @@
 def main():
     import random
-    dice_rolls = 3;
+    dice_rolls = 2;
     roll = [];
-    print(roll)
     for i in range(0,dice_rolls):
-        roll.append(random.randint(1,6));
+        roll.append(random.randint(1,20));
     print(f'You rolled {roll}')
+    for x in roll:
+        if x==1:
+            print (f'You rolled a {x}! Critical fail!')
 
 if __name__== "__main__":
   main()
