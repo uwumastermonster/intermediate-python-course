7816e1b1c2046cab033fcaf64d05cb44ccf5ccb7
uwumastermonster <oscaritue@gmail.com>
2021-10-30 18:44:58 +0200

    Now rolling some dice

--- dice_roller.py
+++ dice_roller.py
@@ -1,5 +1,11 @@
 def main():
-  print('You rolled a die')
+    import random
+    dice_rolls = 3;
+    roll = [];
+    print(roll)
+    for i in range(0,dice_rolls):
+        roll.append(random.randint(1,6));
+    print(f'You rolled {roll}')
 
 if __name__== "__main__":
   main()
